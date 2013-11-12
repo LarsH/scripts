999e4c79b973230e333973145b3f372c55ec952e
Lars Haulin <lars@haulin.se>
2013-11-12 17:38:01 +0100

    Added PS-support for booklet input files

--- printBooklet.sh
+++ printBooklet.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-if ! file "$1" | grep "$1: PDF document" > /dev/null ; then
-   echo "$1 is not a pdf file"
-   exit
-fi
-
-echo Printing \"$1\" as a booklet
-
 #Create temporary directory
 AAA=/tmp/printFolder
-mkdir $AAA
+mkdir -p $AAA
 
-# Convert to ps
-pdf2ps "$1" $AAA/1.ps || exit
+if file "$1" | grep "$1: PDF document" > /dev/null ; then
+   # Argument is a pdf-file, convert
+   pdf2ps "$1" $AAA/1.ps || exit
+else
+      if file "$1" | grep "$1: PostScript document" > /dev/null ; then
+         # Argument is a ps-file, copy
+         cp "$1" $AAA/1.ps || exit
+      else
+         echo "$1 is not a pdf/ps file"
+         exit
+      fi
+fi
+
+
+echo Printing \"$1\" as a booklet
 
 # Auto-stapling works for max 15 sheets, limit to 15*4=60 pages
 psselect -p1-60 $AAA/1.ps $AAA/1.5.ps
